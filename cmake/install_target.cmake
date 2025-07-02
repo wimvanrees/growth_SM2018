f941015e639990d6c846d19e7d4d448b43bd1659
Wim van Rees <wvanrees@mit.edu>
2025-07-02 12:43:22 -0400

    update the cmake file inline with modern cmake

--- cmake/install_target.cmake
+++
@@ -1,8 +0,0 @@
-# install binary
-install(TARGETS shell DESTINATION Elasticity/bin)
-
-# install libs
-install(TARGETS hlbfgs DESTINATION Elasticity/lib)
-
-# install headers
-##install(FILES ${PROJECT_SOURCE_DIR}/src/mylib.h DESTINATION Elasticity/include)
